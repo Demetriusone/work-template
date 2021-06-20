1ece0dce4892f56222cccc6b483657aa8d5be090
Demetrius <wh46tesmoke@gmail.com>
2021-06-20 18:17:33 +0300

    v0.1

---
+++ src/js/Common.js
@@ -0,0 +1,55 @@
+import objectFitImages from 'object-fit-images';
+import objectFitVideos from 'object-fit-videos';
+import { $scrolledElements, $body, Resp, detectIE } from './_helpers';
+
+
+
+
+
+export class Common {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    objectFitImages();
+    objectFitVideos();
+    this.screenScroll();
+    this.expandText();
+    this.addClassIE();
+  }
+
+  addClassIE() {
+    if (detectIE()) $body.addClass('is-ie');
+  }
+
+  screenScroll() {
+    const $btn = $('.screen__descr-btn');
+
+    $btn.on('click', function () {
+      const offsetTop = Resp.isDesk ? 70 : 60;
+      $scrolledElements.animate({
+        scrollTop: $('#pass').offset().top - offsetTop
+      }, 2000);
+      return false;
+    });
+  }
+
+  expandText() {
+    const $btn = $('.js-expand-btn');
+
+    $btn.each(function (i, $btn) {
+      $($btn).on('click', (e) => {
+        const $this = $(e.currentTarget);
+        $this.parent().prev().slideDown();
+        $this.parent().addClass('hidden');
+      });
+    });
+  }
+}
+
+export default new Common();
+
+
+
+
